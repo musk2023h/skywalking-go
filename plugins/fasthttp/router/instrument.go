e5344a866ec01d44d9d3ee38a29860bdbe07562b
moore <wangelectronic77@gmail.com>
2023-12-09 17:59:20 +0800

    Revert "t4"
    This reverts commit c0e4bf5c3c986ba95800d72eadbd945c296b3b2b.

--- plugins/fasthttp/router/instrument.go
+++ plugins/fasthttp/router/instrument.go
@@ -39,7 +39,7 @@ func (i *Instrument) Name() string {
 }
 
 func (i *Instrument) BasePackage() string {
-	return "github.com/buaazp/fasthttprouter"
+	return "github.com/fasthttp/router"
 }
 
 func (i *Instrument) VersionChecker(version string) bool {
